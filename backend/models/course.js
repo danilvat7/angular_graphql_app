3c785169f1e5688a435aaa551e1a5dd1bf50e85b
Kirill Styopkin (3P) <kirill.styopkin@citrix.com>
2018-05-29 18:57:38 +0300

    deploy

---
+++ backend/models/course.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+    id: String,
+    title: String,
+    author: String,
+    description: String,
+    topic: String,
+    url: String,
+    voteCount: {
+        type: Number,
+        default: 0
+    }
+});
+
+courseSchema.index({
+    '$**': 'text'
+});
+
+const model = mongoose.model('course', courseSchema);
+export default model;
